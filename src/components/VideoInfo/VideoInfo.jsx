24dab6f785d0a55b088e470229f2623e9a5e87a9
karansahi001 <iamkaransahi@gmail.com>
2023-05-19 18:02:02 -0400

    Hero section complete for mobile screen sizes

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -1,22 +1,35 @@
+import viewsIcon from '../../assets/images/icons/views.svg'
+import likesIcon from '../../assets/images/icons/likes.svg'
+import './VideoInfo.scss'
 
 
-const VideoInfo = () => {
+const VideoInfo = ({ videoDetails }) => {
+
+    const date = new Date(videoDetails.timestamp);
+    const dateFormat = `${((date.getMonth()+1) < 10 ? `0${date.getMonth()+1}`: (date.getMonth()+1))}/${(date.getDate() < 10 ? `0${date.getDate()}`: date.getDate())}/${date.getFullYear()}`
+
   return (
     <section className="v-info">
-        <h1 className="v-info__head">{}</h1>
+        <h1 className="v-info__head">{videoDetails.title}</h1>
         <hr className="v-info__divider" />
-        <div className="vcontain">
+        <div className="v-container">
             <div className="vcontain">
-                <p className="vcontain__by"></p>
-                <p className="vcontain__date"></p>
+                <p className="vcontain__by">By {videoDetails.channel}</p>
+                <p className="vcontain__date">{dateFormat}</p>
             </div>
             <div className="vcontain">
-                <p className="vcontain__views"><i></i></p>
-                <p className="vcontain__likes"><i></i></p>
+                <p className="vcontain__views">
+                    <img className="vcontain__icons" src={viewsIcon} alt="views eye icon" />
+                    {videoDetails.views}
+                </p>
+                <p className="vcontain__likes">
+                    <img className="vcontain__icons" src={likesIcon} alt="likes heart icon" />
+                    {videoDetails.likes}
+                </p>
             </div>
         </div>
         <hr className="v-info__divider" />
-        <p className="v-info__desc"></p>
+        <p className="v-info__desc"> {videoDetails.description} </p>
     </section>
   )
 }
